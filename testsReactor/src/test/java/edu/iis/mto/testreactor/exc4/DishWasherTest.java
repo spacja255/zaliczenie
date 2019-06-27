20c1644555a762a4b2788c5275abad0bdc16e1cd
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2019-06-27 14:58:19 +0200

    test class added

---
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc4/DishWasherTest.java
@@ -0,0 +1,15 @@
+package edu.iis.mto.testreactor.exc4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DishWasherTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
