83786acf54331f31272b0813009891f32bc0562b
frąg.przemysław <215719@edu.p.lodz.pl>
2020-06-09 18:01:59 +0200

    First test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -1,23 +1,22 @@
 package edu.iis.mto.testreactor.dishwasher;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
 
 import edu.iis.mto.testreactor.dishwasher.engine.Engine;
 import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 
 public class DishWasherTest {
-	@Mock
-	private WaterPump waterPump;
-	@Mock
-	private Engine engine;
-	@Mock
-	private DirtFilter dirtFilter;
-	@Mock
-	private Door door;
+	private WaterPump waterPump = mock(WaterPump.class);
+	private Engine engine = mock(Engine.class);
+	private DirtFilter dirtFilter = mock(DirtFilter.class);
+	private Door door = mock(Door.class);
 	
 	private DishWasher dishWasher;
 	
@@ -29,5 +28,18 @@ public class DishWasherTest {
     @Test
     public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
+    }
+    
+    @Test
+    public void test() {
+    	when(door.closed()).thenReturn(true);
+    	when(dirtFilter.capacity()).thenReturn(0.0);
+    	
+        RunResult result = dishWasher.start(ProgramConfiguration.builder().withFillLevel(FillLevel.HALF).withProgram(WashingProgram.ECO).withTabletsUsed(false).build());
+        
+        RunResult expected = RunResult.builder().withRunMinutes(WashingProgram.ECO.getTimeInMinutes()).withStatus(Status.SUCCESS).build();
+        
+        assertEquals(expected.getRunMinutes(), result.getRunMinutes());
+        assertEquals(expected.getStatus(), result.getStatus());
     }
 }
