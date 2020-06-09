ed2450c9d7d35d5dd7d457b09bda0d8749bc3cbb
user <user@debian-pdsi>
2020-06-09 17:39:32 +0200

    Initialization

--- testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/dishwasher/DishWasherTest.java
@@ -1,12 +1,31 @@
 package edu.iis.mto.testreactor.dishwasher;
 
 import static org.junit.Assert.assertThat;
-
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+
+import edu.iis.mto.testreactor.dishwasher.engine.Engine;
+import edu.iis.mto.testreactor.dishwasher.pump.WaterPump;
 
 public class DishWasherTest {
-
+	@Mock
+	private WaterPump waterPump;
+	@Mock
+	private Engine engine;
+	@Mock
+	private DirtFilter dirtFilter;
+	@Mock
+	private Door door;
+	
+	private DishWasher dishWasher;
+	
+	@Before
+	public void setup() {
+		dishWasher = new DishWasher(waterPump, engine, dirtFilter, door);
+	}
+	
     @Test
     public void itCompiles() {
         assertThat(true, Matchers.equalTo(true));
